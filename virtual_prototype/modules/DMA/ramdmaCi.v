688ed8bd337e81caae44a53d356701003357da76
Gilles Regamey <gillesoff@gmail.com>
2024-04-16 15:14:20 +0200

    with correct architecture

--- virtual_prototype/modules/DMA/ramdmaCi.v
+++ virtual_prototype/modules/DMA/ramdmaCi.v
@@ -5,36 +5,54 @@ module ramDmaCi #(parameter[7:0] customId = 8'h00)
                 input wire[7:0]     ciN,
                 output wire         done,
                 output wire[31:0]   result);
-// internal registers
-reg[31:0] memory [511:0];
-reg[8:0] ptrA;
-// state registers
-reg busy = 0;
+// internal components
+wire[31:0]  dataA,dataB,outA,outB;
+wire[8:0]   addrA,addrB;
+wire        writeA,writeB;
+
+// internal dualport sram
+dualPortSSRAM #() internalmem ( .clockA(clock),
+                            .clockB(clock),
+                            .writeEnableA(writeA),
+                            .writeEnableB(writeB),
+                            .addressA(addrA),
+                            .addressB(addrB),
+                            .dataInA(dataA),
+                            .dataInB(dataB),
+                            .dataOutA(outA),
+                            .dataOutB(outB));
+
+
+// bus interface
+assign writeB = 1'b0;
+assign dataB = 32'd0;
+assign addrB = 8'd0;
+
+//CPU interface
+reg isreading;
+reg[31:0] sramout;
+wire isvalid = (valueA[31:10] == 22'd0) ? 1'b1 : 1'b0;
 
 wire s_isMyCi = (ciN == customId) ? start : 1'b0;
-wire writea_en = valueA[9];                 // 1 bit write enabled
-wire validA = (valueA[31:10] == 22'd0);     // last 22bits must be 0 to write
+assign done = s_isMyCi && !isreading;
 
-assign done = s_isMyCi & !busy; // not always true ?
+assign writeA = valueA[9] && isvalid;
+assign addrA = valueA[8:0];
+assign dataA = valueB;
 
-always @(posedge clock)
-begin
-    if(busy)
-    begin
-        memory[ptrA] = valueA;
-        busy <= 1'b0;
+
+@(posedge clock)begin
+    if(reset || writeA)begin
+         isreading = 1'b0;
+         sramout = 32'b0;
     end
-    else ptrA <= valueA[8:0];
+    if(!writeA && !isreading) isreading = 1'b1;
+    else isreading = 1'b0;
 
-    if(writea_en & validA) busy = 1'b1;
-
-    if(reset == 1'b1)begin
-        for(integer i = 0 ; i < 512 ; i=i+1 ) begin
-            memory[i] = 32'd0;
-        end
-    end
+    if(isreading) sramout = outA;
 end
 
-assign result = (s_isMyCi) ? memory[ptrA] : 32'd0;
+
+
 
 endmodule
