aca08f21db37f4eff5a174be50fbec988c62bab5
Mathieu Schertenleib <schertenleib.mathieu@gmail.com>
2024-04-28 21:20:26 +0200

    Config registers

--- virtual_prototype/modules/DMA/verilog/ramDmaCi.v
+++ virtual_prototype/modules/DMA/verilog/ramDmaCi.v
@@ -11,14 +11,21 @@ module ramDmaCi #(
     output wire [31:0] result
 );
   wire active = (ciN == customId) ? start : 1'b0;
-  wire address_A_valid = (valueA[31:10] == 22'd0);
-  wire write_enable = active & valueA[9] & address_A_valid;
-  wire read_enable = active & !valueA[9] & address_A_valid;
+  wire valueA_valid = (valueA[31:13] == 19'b0);
+  wire write_enable = active & (valueA[12:10] == 3'b0) & valueA[9] & valueA_valid;
+  wire read_enable = active & (valueA[12:10] == 3'b0) & ~valueA[9] & valueA_valid;
+
+  reg [31:0] bus_start_address = 32'b0;
+  reg [8:0] memory_start_address = 9'b0;
+  reg [9:0] block_size = 10'b0;
+  reg [7:0] burst_size = 8'b0;
+  reg [1:0] status_register = 2'b0;
+  reg [1:0] control_register = 2'b0;
 
   wire [31:0] data_out_A;
-  wire [8:0] address_A = (active & address_A_valid) ? valueA[8:0] : 9'b0;
+  wire [8:0] address_A = (active & valueA_valid) ? valueA[8:0] : 9'b0;
 
-  reg data_ready = 1'b0;
+  reg read_data_ready = 1'b0;
 
   dualPortSSRAM #(
       .bitwidth(32),
@@ -26,7 +33,7 @@ module ramDmaCi #(
       .readAfterWrite(0)
   ) ssram (
       .clockA(clock),
-      .clockB(1'b0),
+      .clockB(~clock),
       .writeEnableA(write_enable),
       .writeEnableB(1'b0),
       .addressA(address_A),
@@ -37,13 +44,28 @@ module ramDmaCi #(
       .dataOutB()
   );
 
-  assign done   = data_ready | write_enable | (active & !address_A_valid);
-  assign result = data_ready ? data_out_A : 32'b0;
-
   always @(posedge clock) begin
-    if (read_enable) data_ready <= 1'b1;
-    if (done) data_ready <= 1'b0;
+    if (read_enable) read_data_ready <= 1'b1;
+    if (done) read_data_ready <= 1'b0;
+    if (active & valueA_valid & valueA[9]) begin
+      if (valueA[12:10] == 3'b001) bus_start_address <= valueB[31:0];
+      else if (valueA[12:10] == 3'b010) memory_start_address <= valueB[8:0];
+      else if (valueA[12:10] == 3'b011) block_size <= valueB[9:0];
+      else if (valueA[12:10] == 3'b100) burst_size <= valueB[7:0];
+      else if (valueA[12:10] == 3'b101) control_register <= valueB[1:0];
+    end
   end
+
+  assign done = read_data_ready | (active & ~read_enable);
+
+  assign result = (active & valueA_valid & ~valueA[9]) ?
+      ((valueA[12:10] == 3'b001) ? bus_start_address :
+      (valueA[12:10] == 3'b010) ? memory_start_address :
+      (valueA[12:10] == 3'b011) ? block_size :
+      (valueA[12:10] == 3'b100) ? burst_size :
+      (valueA[12:10] == 3'b101) ? status_register : 32'b0)
+    : read_data_ready ? data_out_A : 32'b0;
+
 
 endmodule
 
--- virtual_prototype/modules/DMA/verilog/ramDmaCi_tb.v
+++ virtual_prototype/modules/DMA/verilog/ramDmaCi_tb.v
@@ -43,12 +43,12 @@ module ramDmaCi_tb;
       if ((done == expDone) && (result == expRes)) begin
         $write("\033[1;32m");
         $display(
-            "[PASSED] start=%b, ciN=%0d, valueA=%0h, valueB=%0h => done=%b (exp %b), result=%0h (exp %0h)",
+            "[PASSED] start=%b, ciN=%0d, valueA=0x%0h, valueB=0x%0h => done=%b (exp. %b), result=0x%0h (exp. 0x%0h)",
             s_start, s_ciN, s_valueA, s_valueB, done, expDone, result, expRes);
       end else begin
         $write("\033[1;31m");
         $display(
-            "[FAILED] start=%b, ciN=%0d, valueA=%0h, valueB=%0h => done=%b (exp %b), result=%0h (exp %0h)",
+            "[FAILED] start=%b, ciN=%0d, valueA=0x%0h, valueB=0x%0h => done=%b (exp. %b), result=0x%0h (exp. 0x%0h)",
             s_start, s_ciN, s_valueA, s_valueB, done, expDone, result, expRes);
       end
       $write("\033[0m");
@@ -58,11 +58,13 @@ module ramDmaCi_tb;
 
   initial begin
     // Check that instruction only activates on start and correct ciN
+    $display("Activation");
     test(.start(1'b0), .ciN(8'd7), .valA(32'h0), .valB(32'h0), .expDone(1'b0), .expRes(32'h0));
     test(.start(1'b1), .ciN(8'd7), .valA(32'h0), .valB(32'h0), .expDone(1'b0), .expRes(32'h0));
     test(.start(1'b0), .ciN(8'd14), .valA(32'h0), .valB(32'h0), .expDone(1'b0), .expRes(32'h0));
 
     // Write then read at address 0
+    $display("Write/read address 0");
     test(.start(1'b1), .ciN(8'd14), .valA(32'h200), .valB(32'h42), .expDone(1'h1), .expRes(32'h0));
     @(negedge clock);
     test(.start(1'b1), .ciN(8'd14), .valA(32'h000), .valB(32'h0), .expDone(1'h0), .expRes(32'h0));
@@ -71,11 +73,41 @@ module ramDmaCi_tb;
     @(negedge clock);
 
     // Write then read at a random address
+    $display("Write/read address 0x37");
     test(.start(1'b1), .ciN(8'd14), .valA(32'h237), .valB(32'h57), .expDone(1'b1), .expRes(32'h0));
     @(negedge clock);
     test(.start(1'b1), .ciN(8'd14), .valA(32'h037), .valB(32'h0), .expDone(1'b0), .expRes(32'h0));
     @(negedge clock);
     test(.start(1'b0), .ciN(8'd0), .valA(32'h037), .valB(32'h0), .expDone(1'b1), .expRes(32'h57));
+    @(negedge clock);
+
+    // Test bus start address
+    $display("Bus start address");
+    test(1'b1, 8'd14, 4'b0011 << 9, 32'h29, 1'b1, 32'h0);
+    @(negedge clock);
+    test(1'b1, 8'd14, 4'b0010 << 9, 32'h0, 1'b1, 32'h29);
+    @(negedge clock);
+
+    // Test memory start address
+    $display("Memory start address");
+    test(1'b1, 8'd14, 4'b0101 << 9, 32'h15, 1'b1, 32'h0);
+    @(negedge clock);
+    test(1'b1, 8'd14, 4'b0100 << 9, 32'h0, 1'b1, 32'h15);
+    @(negedge clock);
+
+    // Test block size
+    $display("Block size");
+    test(1'b1, 8'd14, 4'b0111 << 9, 32'h34, 1'b1, 32'h0);
+    @(negedge clock);
+    test(1'b1, 8'd14, 4'b0110 << 9, 32'h0, 1'b1, 32'h34);
+    @(negedge clock);
+
+    // Test burst size
+    $display("Burst size");
+    test(1'b1, 8'd14, 4'b1001 << 9, 32'h16, 1'b1, 32'h0);
+    @(negedge clock);
+    test(1'b1, 8'd14, 4'b1000 << 9, 32'h0, 1'b1, 32'h16);
+    @(negedge clock);
 
     $finish;
   end
